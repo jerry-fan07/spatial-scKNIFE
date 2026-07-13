5cd3a6bbf8c257b968e2f1dfd176108811ae394a
jerry-fan07 <jerry_fan@brown.edu>
2026-07-13 16:33:57 -0400

    deduplication for cellchat

--- scKNIFE_graph/src/scKNIFE_graph/etl/cellchat.py
+++ scKNIFE_graph/src/scKNIFE_graph/etl/cellchat.py
@@ -10,7 +10,7 @@ warnings.filterwarnings("ignore",
 def extract():
     cc_data = rdata.read_rda(RAW_DIR / "CellChatDB.human.rda") # type = dict
     df: pd.DataFrame = cc_data["CellChatDB.human"]["interaction"] # type = pd.DataFrame
-    df = df[df["annotation"] != "Non-protein Signaling"]
+    df = df[(df["annotation"] != "Non-protein Signaling") & (df["ligand"] != df["receptor"])]
     edge_list: pd.DataFrame = df[["ligand", "receptor"]]
     edge_list.columns = ["src_id", "dst_id"]
     edge_list["edge_type"] = "ligand-receptor"
