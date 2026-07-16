f94cb6f269fc52de3df8560e20a16cfe9ce02506
jerry-fan07 <jerry_fan@brown.edu>
2026-07-16 09:55:28 -0400

    tabula work bug fixed, slightly more data cleaning needed

--- scKNIFE_graph/src/scKNIFE_graph/etl/tabula.py
+++ scKNIFE_graph/src/scKNIFE_graph/etl/tabula.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import pandas as pd
-import numpy as np
 from . import utils
 from ..config import RAW_DIR, SEP_DIR
 
@@ -18,30 +17,31 @@ def extract():
                 with open(file, "r") as f:
                     next(f)
                     cluster_map[prefix] = {
-                        int((parts := line.split(','))[0].strip('"')): parts[1].strip().strip('"')
+                        (int((parts := line.split(','))[0].strip('"'))-1): parts[1].strip().strip('"')
                         for line in f
-                        if line.strip()  # 👈 Crucial: skips empty lines and prevents crashes!
+                        if line.strip()  # skips empty lines and prevents crashes
                     }
                     
     for file in Path(RAW_DIR / "22_markers").iterdir():
         suffix = file.stem[-7:] 
         prefix = file.stem[:-7]
         if (suffix == "markers" and prefix in cluster_map):
-            arr = cluster_map[prefix]
             df = pd.read_csv(file, sep=",", usecols = [6, 7], header=0)
-            df["cluster"] = df["cluster"].map(cluster_map)
+            #print(df["cluster"].unique(), prefix)
+            df["cluster"] = df["cluster"].map(cluster_map[prefix])
             df_list.append(df)
 
-    #df = pd.concat(df_list)
-
-    print(cluster_map)
-    #print(cluster_map["droplet_Bladder_cell_ontology_class_"])
-            
-
     df = pd.concat(df_list)
+    df = pd.DataFrame({
+        "src_id": df["gene"],
+        "dst_id": df["cluster"].map(utils.normalize),
+        "edge_type": "celltype-marker",
+        "source": "tabula"
+    })
     df.to_csv(SEP_DIR / "tabula.tsv", sep='\t', index=False)
-    utils.compile_list(df)
+    utils.add_nodes(df)
 
     return df
-    
-extract()
+
+if __name__ == "__main__":
+    extract()
